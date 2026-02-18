77f34ddc7dea4d3f35feac9cd01bed9d28c36bbe
Alireza Rasouli <77881348+alireza-rasouli@users.noreply.github.com>
2026-02-18 09:36:53 +0330

    Update element-ui.js

--- src/plugins/element-ui.js
+++ src/plugins/element-ui.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Element from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/zh-CN'
+import locale from 'element-ui/lib/locale/lang/en'
 // import '@/assets/css/element-ui.scss'
 // import '@/assets/css/element-element-variables.scss'
 
